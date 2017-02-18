33859620eec525668fbb959968b676a3de97cb2c
abhimanyu98986@gmail.com <Mannu Gupta>
2017-02-18 17:03:17 +0530

    some more changes

---
+++ Shipping/monk/forms.py
@@ -0,0 +1,3 @@
+from django import forms as forms
+from django.forms import ModelForm
+from .models import orderDetail,shop
--- Shipping/monk/models.py
+++ Shipping/monk/models.py
@@ -2,4 +2,22 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.core.validators import RegexValidator
+
 # Create your models here.
+
+class orderDetail(models.Model):
+	Name = models.CharField(max_length=50)
+	timestamp = models.DateTimeField(auto_now_add = True,auto_now=False)
+	Address = models.CharField(max_length=200)
+	orderId = models.CharField(max_length=200)
+
+
+class shop(models.Model):
+	hname = models.CharField(max_length=20)
+	hubId = models.CharField(max_length=10)
+	address = models.CharField(max_length=30)
+	Auth_to = models.CharField(max_length=10)
+	email = models.EmailField()
+	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+	phone_number = models.CharField(validators=[phone_regex], blank=True,max_length=12) #validators should be a list
